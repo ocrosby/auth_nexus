d11179e4bceb1ad8c79de588da0a0ff5973737da
Omar Crosby <omar.crosby@gmail.com>
2024-07-24 06:28:05 -0400

    chore: refactor

---
+++ internal/services/resource/routes.go
@@ -0,0 +1,14 @@
+package router
+
+import "net/http"
+
+func SetupRoutes() *http.ServeMux {
+	router := http.NewServeMux()
+
+	// Example handler
+	router.HandleFunc("GET /whatever", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Example handler"))
+	})
+
+	return router
+}
